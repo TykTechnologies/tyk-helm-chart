b7f1db10266be9e05b041168df7f6f6a14a384b6
gowtham <45805296+abhishiekgowtham@users.noreply.github.com>
2021-07-09 22:49:49 +0800

    [TD-256] Replace wait-for-it script (#138)
    * replace wait-for-it script with while loop
    
    * remove additional space
    
    Co-authored-by: Jack of All Trades <45805296+jack-of-AllTrades@users.noreply.github.com>

--- tyk-pro/scripts/init.sh
+++ tyk-pro/scripts/init.sh
@@ -6,5 +6,5 @@ apk --no-cache add curl jq
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-/opt/scripts/wait-for-it.sh -t 120 $TYK_DASHBOARD_SVC.$TYK_POD_NAMESPACE.svc.cluster.local:$TYK_DB_LISTENPORT && \
+while [[ $(kubectl get po -n $TYK_POD_NAMESPACE -l app=dashboard-tyk-pro -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do sleep 10; done && \
 /opt/scripts/bootstrap.sh
