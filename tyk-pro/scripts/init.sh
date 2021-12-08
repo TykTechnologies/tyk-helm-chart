e283444e7a9cfd093640cee120d965264e51b747
Raman <mail.raman.s@gmail.com>
2021-12-08 15:16:39 +0100

    Fix command in init script to wait for dashboard readiness (#176)

--- tyk-pro/scripts/init.sh
+++ tyk-pro/scripts/init.sh
@@ -6,5 +6,5 @@ apk --no-cache add curl jq
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-while [[ $(kubectl get po -n $TYK_POD_NAMESPACE -l app=dashboard-tyk-pro -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do sleep 10; done && \
+while [[ $(kubectl get pod -n $TYK_POD_NAMESPACE -l app=$TYK_DASHBOARD_DEPLOY -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do sleep 10; done && \
 /opt/scripts/bootstrap.sh
